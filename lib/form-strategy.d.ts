5fc2439dd2ec1f5bc7ff8fe6974f1b42c8cbd15e
Mr_Mao <wwu710632@gmail.com>
2020-09-20 00:09:35 +0800

    newlyAdded: 补充 d.ts 说明

--- lib/form-strategy.d.ts
+++ lib/form-strategy.d.ts
@@ -1,12 +1,17 @@
 /** 校验规则配置 */
 interface ValidateOptions {
+  /** 校验器 */
   validate(value: any, params?: any): boolean;
+  /** 提示消息 */
   message: string | ((params: any) => string);
+  /** 提示别名 */
   name?: string;
 }
 /** 校验规则结果 */
 interface ValidateResult {
+  /** 校验是否成功 */
   validate: boolean;
+  /** 错误信息 */
   error: string;
 }
 
