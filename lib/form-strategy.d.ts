fd0de795d4707f6ac6dc377f87da9210fdd204df
TuiMao233 <7693137+TuiMao233@user.noreply.gitee.com>
2020-09-25 13:36:53 +0800

    class-strategy 模式添加测试

---
+++ lib/form-strategy.d.ts
@@ -0,0 +1,36 @@
+/** 校验规则配置 */
+interface ValidateOptions {
+  /** 校验器 */
+  validate(value: any, params?: any): boolean;
+  /** 提示消息 */
+  message: string | ((params: any) => string);
+  /** 提示别名 */
+  name?: string;
+}
+/** 校验规则结果 */
+interface ValidateResult {
+  /** 校验是否成功 */
+  validate: boolean;
+  /** 错误信息 */
+  error: string;
+}
+
+type Extend = (type: string, options: ValidateOptions) => void;
+type Validate = (type: string, value: any, name?: string, params?: any) => ValidateResult;
+type ValidateAll = (...args: Array<[string, any, string?, any?]>) => ValidateResult;
+interface FormStrategy {
+  extend: Extend;
+  validate: Validate;
+  validateAll: ValidateAll;
+}
+
+/** 添加校验规则 */
+export const extend: Extend;
+/** 进行单次校验 */
+export const validate: Validate;
+/** 进行多次校验 */
+export const validateAll: ValidateAll;
+
+/** 表单校验对象 */
+declare const formStrategy: FormStrategy
+export default formStrategy
---
+++ test/modular-strategy.test.js
@@ -0,0 +1,66 @@
+import { validate, validateAll, extend } from "../lib/form-strategy"
+describe("modular-strategy", ()=> {
+  test("单次验证成功", () => {
+    const status = validate("email", "9561416545@qq.com")
+    expect(status).toEqual({
+      validate: true,
+      error: ""
+    })
+  })
+  test("多次验证成功", () => {
+    const status = validateAll(
+      ["email", "9561416545@qq.com", "邮箱"],
+      ["empty", "毛先生", "名称"]
+    )
+    expect(status).toEqual({
+      validate: true,
+      error: ""
+    })
+  })
+  test("单次验证失败", () => {
+    const status = validate("email", "956141654.com")
+    expect(status).toEqual({
+      validate: false,
+      error: "邮箱格式不正确"
+    })
+  })
+  test("多次验证失败", () => {
+    const status = validateAll(
+      ["email", "9561416545@qq.com", "邮箱"],
+      ["empty", "", "名称"]
+    )
+    expect(status).toEqual({
+      validate: false,
+      error: "名称不能为空"
+    })
+  })
+  test("添加新规则验证, 并通过规则", () => {
+    extend("phone", {
+      validate(value) {
+        return /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$/.test(value)
+      },
+      message: "{__field__}格式不正确",
+      name: "手机号"
+    })
+    const status = validate("phone", "17324162579", "手机号")
+    expect(status).toEqual({
+      validate: true,
+      error: ""
+    })
+  })
+  test("添加规则时, 传递参数", () => {
+    extend("max", {
+      validate(value, params) {
+        return value.length <= params
+      },
+      message: (params) => {
+        return `{__field__}超出了${params}个长度限制`
+      }
+    })
+    const status = validate("max", "12123131231231231", "手机号", 11)
+    expect(status).toEqual({
+      validate: false,
+      error: "手机号超出了11个长度限制"
+    })
+  })
+})
