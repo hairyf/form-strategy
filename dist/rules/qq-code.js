b329f9c630776f697eabd94d94a72e8278ea9e6c
TuiMao233 <wwu710632@gmail.com>
2021-01-19 11:46:28 +0800

    📦 NEW: 修改文件。

--- dist/rules/qq-code.js
+++ dist/rules/qq-code.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Mr.Mao
- * @LastEditors: Mr.Mao
- * @Date: 2020-10-12 08:47:52
- * @LastEditTime: 2020-12-08 15:03:42
- * @Description:
- * @任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
- */
 export default {
     validate(value) {
         return /^[1-9][0-9]{4,10}$/.test(value);
